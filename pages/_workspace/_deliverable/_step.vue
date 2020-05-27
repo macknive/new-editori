c92a9ef9a60b41f8113784212f05646b640ca6ad
Makus Nikomi <admin@nikomi.co>
2020-05-26 21:20:18 -0400

    Auto-save deliverables on change.

--- pages/_workspace/_deliverable/_step.vue
+++ pages/_workspace/_deliverable/_step.vue
@@ -8,6 +8,7 @@
           :key="componentData.id"
           :is="componentData.component.name"
           :deliverable="deliverable"
+          @autoSave="autoSave"
           :style="`
             grid-column-start: ${componentData.column_start};
             grid-column-end: span ${componentData.column_span};
@@ -16,6 +17,8 @@
           `">
       </component>
     </div>
+    <SaveIcon :status="saveStatus" class="save-icon"
+        :title="saveTooltip"></SaveIcon>
   </div>
 </template>
 
@@ -23,24 +26,42 @@
 import ArticleEditor from '~/components/ArticleEditor';
 import ContentAnalysis from '~/components/ContentAnalysis';
 import EditorialBrief from '~/components/EditorialBrief';
+import SaveIcon from '~/components/SaveIcon';
 import Workflow from '~/components/Workflow';
 import DeliverableBySlug from '~/queries/DeliverableBySlug';
+import UpdateDeliverable from '~/queries/UpdateDeliverable';
 import WorkspaceBySlug from '~/queries/WorkspaceBySlug';
+import {debounce} from 'vue-debounce';
+
+/**
+ * How long to wait (in milliseconds) after the most recent changes to the
+ * deliverable to perform a save operation.
+ */
+const AUTOSAVE_DEBOUNCE_MS = 3000;
+
+const SaveStatus = {
+  ERROR: 'error',
+  SAVING: 'saving',
+  SAVED: 'saved',
+  WAITING: 'waiting',
+};
 
 export default {
   components: {
     ArticleEditor,
     ContentAnalysis,
     EditorialBrief,
+    SaveIcon,
     Workflow,
   },
   data() {
     return {
-      deliverableSlug: this.$route.params.deliverable,
-      workspaceSlug: this.$route.params.workspace,
-      stepSlug: this.$route.params.step,
       deliverables: [],
+      deliverableSlug: this.$route.params.deliverable,
+      saveStatus: SaveStatus.SAVED,
+      stepSlug: this.$route.params.step,
       workspaces: [],
+      workspaceSlug: this.$route.params.workspace,
     }
   },
   computed: {
@@ -59,6 +80,18 @@ export default {
         grid-template-rows: ${this.stringifyLengths(this.layout.rows)};
         grid-template-columns: ${this.stringifyLengths(this.layout.columns)};
       `;
+    },
+    saveTooltip() {
+      switch (this.saveStatus) {
+        case SaveStatus.ERROR:
+          return 'Not saved - An error has occurred.';
+        case SaveStatus.WAITING:
+          return 'Waiting until you finish typing...';
+        case SaveStatus.SAVING:
+          return 'Saving...';
+        case SaveStatus.SAVED:
+          return 'Saved!';
+      }
     },
     view() {
       return this.currentStep.view;
@@ -91,7 +124,39 @@ export default {
       const value = length.value || '';
       const unit = length.unit === 'percent' ? '%' : length.unit;
       return `${value}${unit}`;
+    },
+    save() {
+      this.saveStatus = SaveStatus.SAVING;
+
+      // TODO: Can this be a constant object with functions for the variables to
+      // prevent frequent object allocation?
+      const mutationConfig = {
+        mutation: UpdateDeliverable,
+        variables: {
+          id: this.deliverable.id,
+          data: this.deliverable.data,
+          title: this.deliverable.title,
+          slug: this.deliverable.slug,
+        }
+      };
+
+      this.$apollo.mutate(mutationConfig)
+          .then(result => this.onSaveSuccess(result))
+          .catch(err => this.onSaveError(err));
+    },
+    onSaveSuccess(result) {
+      this.saveStatus = SaveStatus.SAVED;
+    },
+    onSaveError(err) {
+      this.saveStatus = SaveStatus.ERROR;
     }
+  },
+  created() {
+    const debouncedSave = debounce(() => this.save(), AUTOSAVE_DEBOUNCE_MS);
+    this.autoSave = () => {
+      this.saveStatus = SaveStatus.WAITING;
+      debouncedSave();
+    };
   },
   apollo: {
     deliverables: {
@@ -117,6 +182,9 @@ export default {
 </script>
 
 <style>
+@-webkit-keyframes loading-pulse { 0% { opacity: 0.2; } 100% { opacity: 0.4; } }
+@keyframes loading-pulse         { 0% { opacity: 0.2; } 100% { opacity: 0.4; } }
+
 body {
   background: #f0f0f0;
   --gap: 20px;
@@ -142,5 +210,10 @@ body {
   background: #fff;
   border-radius: calc(var(--gap) / 2);
   padding: var(--gap);
+}
+.save-icon {
+  position: absolute;
+  bottom: 16px;
+  right: 16px;
 }
 </style>
