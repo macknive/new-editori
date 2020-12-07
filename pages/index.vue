bd67463dca05ae13f36427984a2a99b9c36d39d3
Macknive <kevin.corpuz@virtusize.com>
2020-12-07 17:29:43 +0800

    add sample queries on the landing page and list workspace

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>Landing page</h1>
+<div>
+<h1>Landing page</h1>
+List of workspaces
+  <ul>
+    <li v-for="workspaces in workspaces" :key="workspaces.id">
+      <nuxt-link :to="`${workspaces.slug}`" target="_blank">{{workspaces.name}}</nuxt-link>
+    </li>
+  </ul>
+</div>  
+
 </template>
 
 <script>
+import gql from 'graphql-tag'
 export default {
-  
+  apollo: {
+    workspaces: gql 
+      `query GetWorkspaceBySlug {
+      workspaces {
+        id
+        name
+        slug
+      }
+    }`
+  },
 }
 </script>
 
