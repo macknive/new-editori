515f2a626a5888cfec8851cbf7205d2fb9e963e9
Macknive <krc.virtual@gmail.com>
2021-02-09 13:04:13 +0800

    format deadline to moment relative time, make the full row in link

--- components/DeliverableTableRow.vue
+++ components/DeliverableTableRow.vue
@@ -1,46 +1,45 @@
 <template>
-  <v-row class="mb-6">
-    <v-col class="my-auto date">
-      <h4 class="pl-1">{{ updatedAtMonth }}</h4>
-      <h3>{{ updatedAtDate }}</h3>
-    </v-col>
-    <v-col class="my-auto">
-      <v-row no-gutters>
-        <v-col class="status-placeholder" cols="1">
-          <div class="status-green"></div>
-        </v-col>
-        <v-col>
-          <h5>ON TRACK</h5>
-        </v-col>
-      </v-row>
-      <div>
-        <nuxt-link :to="`${baseUrl}/${deliverable.slug}`" class="title">
+  <nuxt-link :to="`${baseUrl}/${deliverable.slug}`" class="title">
+    <v-row class="mb-6">
+      <v-col class="my-auto date">
+        <h4 class="pl-1">{{ updatedAtMonth }}</h4>
+        <h3>{{ updatedAtDate }}</h3>
+      </v-col>
+      <v-col class="my-auto">
+        <v-row no-gutters>
+          <v-col class="status-placeholder" cols="1">
+            <div class="status-green"></div>
+          </v-col>
+          <v-col>
+            <h5>ON TRACK</h5>
+          </v-col>
+        </v-row>
+        <div>
           <div>
             {{ deliverable.title }} <br />
             <span v-if="nextStep.assignee">
               {{ nextStep.assignee.display_name }} is on
-              {{ nextStep.label }} step, agreed to deliver by
-              {{ nextStep.deadline }}.
+              {{ nextStep.label }} step, agreed to deliver {{ deadLine }}.
             </span>
           </div>
-        </nuxt-link>
-      </div>
-    </v-col>
-    <v-col
-      md="2"
-      v-if="nextStep"
-      class="status my-auto btn-right"
-      align="right"
-    >
-      <v-btn v-if="isViewerAssignee" color="brown-btn"
-        >{{ nextStep.label }}
-      </v-btn>
-      <v-btn v-if="!isViewerAssignee" color="grey-btn">PEEK</v-btn>
-    </v-col>
-    <v-col v-else class="status">
-      All done!
-    </v-col>
-  </v-row>
+        </div>
+      </v-col>
+      <v-col
+        md="2"
+        v-if="nextStep"
+        class="status my-auto btn-right"
+        align="right"
+      >
+        <v-btn v-if="isViewerAssignee" color="brown-btn"
+          >{{ nextStep.label }}
+        </v-btn>
+        <v-btn v-if="!isViewerAssignee" color="grey-btn">PEEK</v-btn>
+      </v-col>
+      <v-col v-else class="status">
+        All done!
+      </v-col>
+    </v-row>
+  </nuxt-link>
 </template>
 
 <script>
@@ -65,6 +64,9 @@ export default {
     },
     updatedAtDate() {
       return moment(this.deliverable.updated_at).format('DD')
+    },
+    deadLine() {
+      return moment(this.nextStep.deadline).fromNow()
     },
     isViewerAssignee() {
       if (!this.nextStep.assignee) {
