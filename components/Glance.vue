462d667c21aa4828a2e7429cc56da4c0213916fe
makusnikomi <65748930+makusnikomi@users.noreply.github.com>
2021-04-23 21:55:16 -0400

    Move glance into its own component

---
+++ components/Glance.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="glance">
+    <h2>{{title}}</h2>
+    <div class="figures">
+      <figure class="figure" v-for="(figure, index) in figures" :key="index">
+        {{figure.value}}
+        <figcaption class="caption label">{{figure.caption}}</figcaption>
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'title', 'figures' ]
+}
+</script>
+
+<style>
+.glance {
+  left: 192rem;
+  top: 614rem;
+  border-left: 6rem solid #593D3B;
+  padding-left: 24rem;
+}
+.glance h2 {
+  margin-bottom: 24rem;
+}
+.glance .figures {
+  display: flex;
+  flex-direction: row;
+}
+.figure {
+  font-family: var(--display-font);
+  font-size: 36rem;
+  margin-right: 40rem;
+}
+.caption {
+  font-family: var(--body-font);
+  font-size: 12rem;
+  font-weight: 900;
+  line-height: 1.5;
+  margin-top: 40rem;
+  max-width: 100rem;
+}
+</style>
--- pages/ui.vue
+++ pages/ui.vue
@@ -9,15 +9,7 @@
         <span>Switch workspace</span>
         <font-awesome-icon class="caret" icon="caret-down" />
       </div>
-      <div class="glance">
-        <h2>This Week</h2>
-        <div class="figures">
-          <figure class="figure" v-for="(figure, index) in figures" :key="index">
-            {{figure.value}}
-            <figcaption class="caption label">{{figure.caption}}</figcaption>
-          </figure>
-        </div>
-      </div>
+      <Glance title="This Week" :figures="figures" />
       <img src="~/assets/svgs/editor.svg" alt="" class="editor">
       <button class="editor-button">
         Chat with Editor
@@ -33,6 +25,7 @@
 </template>
 
 <script>
+import Glance from '~/components/Glance';
 import PageTemplate from '~/components/layout/PageTemplate';
 
 const currentTime = new Date();
@@ -49,7 +42,7 @@ function pad(value, totalDigits) {
 }
 
 export default {
-  components: { PageTemplate },
+  components: { Glance, PageTemplate },
   data() {
     return {
       shapes: [
@@ -111,32 +104,6 @@ export default {
     left: 100rem;
     top: 360rem;
     text-transform: uppercase;
-  }
-  .glance {
-    left: 192rem;
-    top: 614rem;
-    border-left: 6rem solid #593D3B;
-    padding-left: 24rem;
-  }
-  .glance h2 {
-    margin-bottom: 24rem;
-  }
-  .glance .figures {
-    display: flex;
-    flex-direction: row;
-  }
-  .figure {
-    font-family: var(--display-font);
-    font-size: 36rem;
-    margin-right: 40rem;
-  }
-  .caption {
-    font-family: var(--body-font);
-    font-size: 12rem;
-    font-weight: 900;
-    line-height: 1.5;
-    margin-top: 40rem;
-    max-width: 100rem;
   }
   .editor {
     left: 795rem;
