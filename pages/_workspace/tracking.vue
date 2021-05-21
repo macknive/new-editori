7f040bfac0e127dd3b72cc9334ccdee732d79b61
Macknive <krc.virtual@gmail.com>
2021-05-21 17:38:33 +0800

    add tracking page

---
+++ pages/_workspace/tracking.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <HeroSection>
+      <div class="title width-100">
+        <div class="container-row m-auto">
+          <div class="width-500 m-auto">
+            <h2>
+              Select pages to track and unlock historical performance, providing
+              uniqe insights, and tailored opportunities to how to grow traffic
+              & revenue
+            </h2>
+          </div>
+          <div class="m-auto width-500 switch-count container-row">
+            <div class="tracked">
+              {{ tracking }}
+            </div>
+            <div class="untracked">/{{ untracked }}</div>
+          </div>
+        </div>
+      </div>
+    </HeroSection>
+    <BodySection>
+      <div class="container-row m-auto mb-20">
+        <div class="width-500">Title</div>
+        <div class="m-auto">Tracking</div>
+      </div>
+
+      <div v-for="page in pages" :key="page.id">
+        <div v-if="page.title" class="container-row">
+          <div class="width-500 mb-20">
+            {{ page.title }}
+            <p>Written by: {{ writer }}</p>
+          </div>
+          <div class="m-auto">
+            <vs-switch v-model="page.trackingType" />
+          </div>
+        </div>
+      </div>
+    </BodySection>
+  </div>
+</template>
+
+<script>
+import BodySection from '~/components/layout/BodySection';
+import HeroSection from '~/components/layout/HeroSection';
+import ListPages from '~/queries/ListPages';
+export default {
+  components: {
+    BodySection,
+    HeroSection
+  },
+  data() {
+    return {
+      pages: [],
+      //color: '#593d3b',
+      writer: 'Meg',
+      trackStatus: ''
+    };
+  },
+  computed: {
+    tracking() {
+      return this.pages.filter(value => value.trackingType === true).length;
+    },
+    untracked() {
+      return this.pages.filter(value => value.trackingType === null || false)
+        .length;
+    }
+  },
+  apollo: {
+    pages: {
+      prefetch: true,
+      query: ListPages
+    }
+  }
+};
+</script>
+
+<style>
+.title {
+  top: 200rem;
+}
+.switch-count {
+  border-radius: 50%;
+  border: 1px solid #593d3b;
+  padding: 22px;
+  height: 200px;
+  width: 200px;
+  margin: auto;
+  background-color: #593d3b;
+}
+.tracked {
+  margin: 60px 0 0 30px;
+  font-size: 24px;
+  color: white;
+}
+
+.untracked {
+  margin-top: 35px;
+  font-size: 48px;
+  color: white;
+}
+</style>
