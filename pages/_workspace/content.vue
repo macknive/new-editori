11f4a9ff9c0631e5a50f0a6510966f7759237822
Macknive <krc.virtual@gmail.com>
2021-06-02 18:57:38 +0800

    fix v-calendar styling

--- pages/_workspace/content.vue
+++ pages/_workspace/content.vue
@@ -1,47 +1,47 @@
 <template>
   <HeroSection>
     <no-ssr>
-      <v-calendar class="calendar"></v-calendar>
+      <v-calendar class="calendar" trim-weeks></v-calendar>
     </no-ssr>
   </HeroSection>
 </template>
 
 <script>
-
-import BodySection from '~/components/layout/BodySection'
-import HeroSection from '~/components/layout/HeroSection'
+import BodySection from '~/components/layout/BodySection';
+import HeroSection from '~/components/layout/HeroSection';
 
 export default {
   components: {
-    BodySection, HeroSection
+    BodySection,
+    HeroSection
   },
   created() {
     this.$store.commit('navbar/setShouldShowWorkspace', true);
-  },
-}
+  }
+};
 </script>
 
 <style scoped>
-  .calendar {
-    background: transparent;
-    border: 0;
-    left: 929rem;
-    top: 228rem;
-    width: 425rem;
-    height: 481rem;
-    font-family: var(--display-font);
-  }
+.calendar {
+  background: transparent;
+  border: 0;
+  left: 929rem;
+  top: 228rem;
+  min-width: 425rem;
+  height: 481rem;
+  font-family: var(--display-font);
+}
 
-  .calendar >>> .vc-title {
-    text-transform: uppercase;
-  }
+.calendar >>> .vc-title {
+  text-transform: uppercase;
+}
 
-  .calendar >>> .vc-day {
-    font-size: 28rem !important;
-  }
+.calendar >>> .vc-day {
+  font-size: 28rem !important;
+}
 
-  .calendar >>> .vc-day.is-not-in-month {
-    font-size: 16rem !important;
-    opacity: 0.4 !important;
-  }
+.calendar >>> .vc-day.is-not-in-month > span {
+  font-size: 16rem !important;
+  opacity: 0.4 !important;
+}
 </style>
