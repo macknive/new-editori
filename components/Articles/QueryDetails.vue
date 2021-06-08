4f148026ac744a252e641b3cd2d384e4ab9038f4
Macknive <krc.virtual@gmail.com>
2021-06-08 13:00:22 +0800

    add query details and multiple chart on post

--- components/Articles/QueryDetails.vue
+++ components/Articles/QueryDetails.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>Query Details Component</div>
+  <div>
+    <div class="container-row mb-20">
+      <div class="m-auto">
+        <h3>top queries</h3>
+
+        <div class="container-row mt-20">
+          <div class="status-decreasing mr-5"></div>
+          <div class="fs-14">LOST</div>
+        </div>
+        <h3>benefits of water in the morning</h3>
+      </div>
+      <div>
+        <h3>average ranking</h3>
+        <div class="mt-20">
+          3
+        </div>
+      </div>
+      <div class="m-auto">
+        <h3>competitors</h3>
+        <div class="container-column mt-20">
+          <h3>#1 why you need to drink water</h3>
+          <h3>#2 my body when i drank water</h3>
+        </div>
+      </div>
+      <div>
+        <span class="material-icons-outlined">
+          filter_list
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- components/Articles/RankingStable.vue
+++ components/Articles/RankingStable.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    Ranking & Stable Component
+    <vs-button :color="color" type="flat" @click="toggleChart = true"
+      >Views</vs-button
+    >
+    <vs-button type="flat" @click="toggleChart = false">Ranking</vs-button>
+    <div v-if="toggleChart">
+      <highcharts :options="viewData"></highcharts>
+    </div>
 
-    <highcharts :options="lineData"></highcharts>
+    <div v-if="!toggleChart">
+      <highcharts :options="rankData"></highcharts>
+    </div>
   </div>
 </template>
 
@@ -10,11 +18,13 @@
 export default {
   data() {
     return {
-      lineArray: [5, 2, 3, 3]
+      toggleChart: true,
+      lineArray: [5, 2, 3, 3],
+      rankArray: [3, 5, 2, 4]
     };
   },
   computed: {
-    lineData() {
+    viewData() {
       return {
         credits: {
           enabled: false
@@ -26,6 +36,22 @@ export default {
           {
             type: 'line',
             data: this.lineArray
+          }
+        ]
+      };
+    },
+    rankData() {
+      return {
+        credits: {
+          enabled: false
+        },
+        title: {
+          text: ''
+        },
+        series: [
+          {
+            type: 'line',
+            data: this.rankArray
           }
         ]
       };
